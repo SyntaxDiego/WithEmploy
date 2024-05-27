893c2ddd7ad205e722bb59b39ac247c42a110384
Diego Carranza <diego300carranza754@gmail.com>
2024-05-26 20:00:38 -0500

    Styles - in process

--- resources/views/livewire/country-livewire.blade.php
+++ resources/views/livewire/country-livewire.blade.php
@@ -1,20 +1,22 @@
 <div>
-    <input type="text" wire:model=searchCountry placeholder="Search for name">
+    <input class="search" type="text" wire:model=searchCountry placeholder="Search for name">
+    <br>
     <table>
         <thead>
-            <tr>
+            <tr class="thead">
                 <th>Name</th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
             @foreach ($countries as $country)
-                <tr>
-                    <td>{{$country->name}}</td>
-                    <td><a href="{{route('country.show', $country)}}">Show</a></td>
-                    <td><a href="{{route('country.edit', $country)}}">Edit</a></td>
+                <tr class="tbody">
+                    <td class="title">{{ $country->name }}</td>
                     <td>
-                        <form action="{{route('country.destroy', $country->id)}}" method="POST" onsubmit="return confirm('Are you sure you want to delete this country?')">
+                        <a href="{{ route('country.show', $country) }}">Show</a>
+                        <a href="{{ route('country.edit', $country) }}">Edit</a>
+                        <form action="{{ route('country.destroy', $country->id) }}" method="POST"
+                            onsubmit="return confirm('Are you sure you want to delete this country?')">
                             @csrf
                             @method('DELETE')
                             <button type="submit">Delete</button>
